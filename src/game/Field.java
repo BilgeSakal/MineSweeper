726f40c66c8eabffad6ec257a0d22eda9a424e55
Sakal <sakal@Sakal-MacBook-Pro.local>
2019-01-14 02:33:15 +0300

    getters and setters implemented

--- src/game/Field.java
+++ src/game/Field.java
@@ -7,10 +7,30 @@ public class Field {
 	
 	private boolean mine;
 	
+	public Field() {
+		this.xPos = 0;
+		this.yPos = 0;
+		this.mine = false;
+	}
+	
 	public Field(int xPos, int yPos, boolean mine) {
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.mine = mine;
+	}
+	
+	public void setXPos(int xPos) throws IndexOutOfBoundsException {
+		if(xPos >= 0)
+			this.xPos = xPos;
+		else
+			throw new IndexOutOfBoundsException();
+	}
+	
+	public void setYPos(int yPos) throws IndexOutOfBoundsException {
+		if(yPos >= 0)
+			this.yPos = yPos;
+		else
+			throw new IndexOutOfBoundsException();
 	}
 	
 	public int getXPos() {
