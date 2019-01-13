ecb77cce6f8bc8979d396b4b589229564184d8df
Sakal <sakal@Sakal-MacBook-Pro.local>
2019-01-14 01:46:56 +0300

    Field class implemented

--- src/game/Field.java
+++ src/game/Field.java
@@ -1,5 +1,28 @@
 package game;
 
 public class Field {
+	
+	private int xPos;
+	private int yPos;
+	
+	private boolean mine;
+	
+	public Field(int xPos, int yPos, boolean mine) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.mine = mine;
+	}
+	
+	public int getXPos() {
+		return xPos;
+	}
+	
+	public int getYPos() {
+		return yPos;
+	}
+	
+	public boolean isMine() {
+		return mine;
+	}
 
 }
